60ab4e4442505af75105f1c53a5777918c21d204
Finger <finger@spianmo.com>
2024-03-18 18:51:20 +0800

    init docs

---
+++ .vuepress/config.ts
@@ -0,0 +1,147 @@
+import {defineUserConfig} from "vuepress";
+import recoTheme from "vuepress-theme-reco";
+
+export default defineUserConfig({
+    title: "Glass Develops Tutorial",
+    description: "Just playing around",
+    theme: recoTheme({
+        style: "@vuepress-reco/style-default",
+        logo: "/logo.png",
+        author: "Finger",
+        authorAvatar: "/head.png",
+        docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
+        docsBranch: "main",
+        docsDir: "example",
+        lastUpdatedText: "",
+        // series 为原 sidebar
+        series: {
+            "/docs/theme-reco/": [
+                {
+                    text: "module one",
+                    children: ["home", "theme"],
+                },
+                {
+                    text: "module two",
+                    children: ["api", "plugin"],
+                },
+            ],
+            '/blogs/': [
+                {
+                    text: 'blogs1',
+                    children: [
+                        '/blogs/deploy.md'
+                    ],
+                },
+            ]
+        },
+        navbar: [
+            {text: '快速开始', link: '/docs/guide', icon: 'Compass'},
+            {
+                text: 'API文档',
+                icon: 'Document',
+                children: [
+                    {
+                        text: '基础框架',
+                        children: [
+                            {text: '模版基类', link: '/docs/基础框架/模版基类.html'},
+                            {text: '交互逻辑', link: '/docs/基础框架/模版基类.html'},
+                            {text: 'UI 组件', link: '/docs/基础框架/UI 组件.html'},
+                            {text: '相机/麦克风', link: '/docs/基础框架/相机_麦克风.html',},
+                            {text: '设计资源', link: '/docs/基础框架/设计资源.html'},
+                        ],
+                    },
+                    {
+                        text: '插件化容器',
+                        children: [
+                            {text: '用户空间', link: '/docs/插件化容器/用户空间.html'},
+                            {text: '插件管理', link: '/docs/插件化容器/插件管理.html'},
+                            {text: '通信机制', link: '/docs/插件化容器/通信机制.html'},
+                            {text: '消息推送', link: '/docs/插件化容器/消息推送.html'},
+                            {text: '插件镜像源', link: '/docs/插件化容器/插件镜像源.html'},
+                        ],
+                    },
+
+                ],
+            },
+            {
+                text: '扩展能力',
+                icon: 'SubVolume',
+                children: [
+                    {text: '远程协助', link: 'https://v2.vuepress.vuejs.org/zh/reference/docs/plugins/vue-previews'},
+                    {
+                        text: 'LLM知识库',
+                        link: 'https://v2.vuepress.vuejs.org/zh/reference/docs/plugins/bulletin-popover'
+                    },
+                    {text: '人脸识别', link: 'https://v2.vuepress.vuejs.org/zh/reference/docs/plugins/page'},
+                    {text: 'OCR组件', link: 'https://v2.vuepress.vuejs.org/zh/reference/docs/plugins/comments'},
+                    {text: 'SOP组件', link: 'https://v2.vuepress.vuejs.org/zh/reference/docs/plugins/bulletin-popover'},
+                ],
+            },
+            {
+                text: '版本路线',
+                icon: 'SubVolume',
+                children: [
+                    {text: '1.x(RC)', link: '/api/1.x/'},
+                ],
+            },
+            {text: '技术博客', link: '/posts', icon: 'DocumentAttachment'},
+            {text: '案例', link: '/docs/examples', icon: 'Fire'},
+            {text: '留言板', link: '/docs/message-board', icon: 'Chat'},
+        ],
+        bulletin: {
+            body: [
+                {
+                    type: "text",
+                    content: `🎉🎉🎉 TH 太昊开发者平台正式发布，并且希望开发者们在 QQ 社群和 GitHub 踊跃反馈使用体验和开发需求，我们会第一时间响应。推出统一开发框架是为了补全XR眼镜的通用能力的同时将魔数团在B端业务上的核心服务沉淀下来共享给全行业，换而言之，你们只管开发，我们来搞定各厂商硬件适配。`,
+                    style: "font-size: 12px;",
+                },
+                {
+                    type: "hr",
+                },
+                {
+                    type: "title",
+                    content: "开发社群",
+                },
+                {
+                    type: "text",
+                    content: `
+          <ul>
+            <li>XR行业交流群：<_待_填_写_></li>
+            <li>XR开发者交流群：<_待_填_写_></li>
+          </ul>`,
+                    style: "font-size: 12px;",
+                },
+                {
+                    type: "hr",
+                },
+                {
+                    type: "title",
+                    content: "GitHub",
+                },
+                {
+                    type: "text",
+                    content: `
+          <ul>
+            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+          </ul>`,
+                    style: "font-size: 12px;",
+                }
+            ],
+        },
+        commentConfig: {
+            type: 'valine',
+            // options 与 1.x 的 valineConfig 配置一致
+            options: {
+                // appId: 'xxx',
+                // appKey: 'xxx',
+                // placeholder: '填写邮箱可以收到回复提醒哦！',
+                // verify: true, // 验证码服务
+                // notify: true,
+                // recordIP: true,
+                // hideComments: true // 隐藏评论
+            },
+        },
+    }),
+    // debug: true,
+});
---
+++ README.md
@@ -0,0 +1,56 @@
+---
+home: true
+modules:
+  - BannerBrand
+  - Features
+  - MdContent
+  - Footer
+bannerBrand:
+  bgImage: '/bg.svg'
+  title: XR-Glass Application Develops Tutorial
+  description: 一套完整、标准的XR应用开发框架。
+  tagline: 旨在为AR行业开发者提供统一的硬件交互接口、为用户在各厂商AR硬件上带来更一致使用体验、加速AR设备厂商构建更丰富的场景生态。框架底层拥有音视频远程协助、SOP、PNS、AI终端推理等核心服务，高效赋能三方开发者进行多场景下AR + AI应用的快速开发。
+  buttons:
+    - { text: 快速开始>_, link: '/docs/guide/introduce' }
+    - { text: 探索下一代XR基座, link: '/docs/style-default-api/introduce', type: 'plain' }
+  socialLinks:
+    - { icon: 'LogoGithub', link: 'https://github.com/vuepress-reco/vuepress-theme-reco' }
+    - { icon: 'LogoWechat', link: 'https://github.com/vuepress-reco/vuepress-theme-reco' }
+    - { icon: 'LogoDiscord', link: '' }
+    - { icon: 'Wikis', link: '' }
+isShowTitleInHome: true
+actionText: About
+actionLink: /views/other/about
+features:
+  - title: 更一致的交互体验
+    details: 统一市场上主流的XR眼镜各类触摸板、旋钮等操作交互，开发者只需关注业务本身，无需关注眼镜硬件底层人机交互。
+  - title: 更成熟的硬件兼容
+    details: 沉淀XR行业多年，已兼容适配多种主流XR眼镜的，包括Rokid、Vuzix、蜂巢Glass、Argooz、拜特尔等，后续将适配更多硬件。
+  - title: 更现代的开发体验
+    details: 提供Java/Kotlin等多种开发语言的SDK，拥有更丰富的DSL语法糖，紧跟现代Android应用开发趋势，提升开发者开发效率。
+---
+
+## 快速开始
+
+:::: code-group
+::: code-group-item Gradle Groovy DSL
+
+```gradle
+dependencies {
+    implementation 'com.teamhelper.xr:glass-sdk:<LatestVersion>'
+    ...
+}
+```
+
+:::
+::: code-group-item Gradle Kotlin DSL
+
+```kotlin
+dependencies {
+    implementation("com.teamhelper.xr:glass-sdk:<LatestVersion>")
+    ...
+}
+```
+
+:::
+::::
