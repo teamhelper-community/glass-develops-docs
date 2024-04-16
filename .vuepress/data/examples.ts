27a2120b061f10b7ae1caa615cd36461a736315e
Finger <finger@spianmo.com>
2024-04-16 16:58:14 +0800

    docs: add develops application case

---
+++ .vuepress/data/examples.ts
@@ -0,0 +1,7 @@
+export default [
+    {
+        thumbnail: "https://teamhelper.cn/static/img/product/product_jsxx/banner.png",
+        link: "https://teamhelper.cn/",
+        avator: "https://resource-1258209752.cos.ap-nanjing.myqcloud.com/teamhelper/team-helper-im/login_logo.png"
+    },
+]
---
+++ docs/examples.md
@@ -0,0 +1,11 @@
+---
+title: 案例
+isShowComments: false
+---
+
+::: tip
+添加案例请点击 [这里](https://github.com/teamhelper-community/glass-develops-docs/edit/master/docs/.vuepress/data/examples.ts)
+编辑完毕后提交PR，审核通过后合并到`Glass Develops`案例中。
+:::
+
+<examples />
